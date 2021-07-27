90987115e01bdf02d74fcde59d9f59a1b2688ef7
jeanmarcj <jmjbup@gmail.com>
2021-07-27 14:25:19 +0200

    Fix. Update controllers for sauce.js

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -14,7 +14,7 @@ exports.createSauce = (req, res, next) => {
         ...sauceObject,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
-    console.log(sauce);
+    // console.log(sauce);
     sauce.save()
     .then(() => res.status(201).json({ message : 'Sauce enregistrée !'}))
     .catch(error => res.status(400).json({error}));
@@ -28,12 +28,14 @@ exports.createSauce = (req, res, next) => {
  */
 exports.modifySauce = (req, res, next) => {
     const sauceObject = req.file ?
-    {
-        ...JSON.parse(req.body.sauce)
-    } : { ...req.body };
-    console.log(sauceObject);
-    console.log('Sauce modifiée, id: ', req.params.id);
-    res.status(200).json({ message: 'Sauce modifiée !'});
+        { ...JSON.parse(req.body.sauce), imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        } : { ...req.body };
+    Sauce.updateOne({ _id: req.params.id}, {...sauceObject, _id: req.params.id})
+    .then(() => res.status(200).json({message: 'Sauce modifiée !'}))
+    .catch(error => res.status(400).json({error}));
+    // console.log(sauceObject);
+    // console.log('Sauce modifiée, id: ', req.params.id);
+    // res.status(200).json({ message: 'Sauce modifiée !'});
 }
 
 /**
@@ -43,9 +45,19 @@ exports.modifySauce = (req, res, next) => {
  * @param {*} next  the next middleware function to execute
  */
 exports.deleteSauce = (req, res, next) => {
-    res.status(200).json({ message: 'Objet supprimé !'});
-    let sauceId = req.params.id;
-    console.log('Route delete. Sauce ID: ' + sauceId);
+    Sauce.findOne({ _id: req.params.id })
+    .then(sauce => {
+        const filename = sauce.imageUrl.split('/images/'[1]);
+        fs.unlink(`images/${filename}`, () => {
+            Sauce.deleteOne({_id: req.params.id})
+            .then(() => res.status(200).json({message: 'Sauce supprimée !'}))
+            .catch(error => res.status(400).json({ error }));
+        });
+    })
+    .catch(error => res.status(500).json({ error }));
+    // res.status(200).json({ message: 'Objet supprimé !'});
+    // let sauceId = req.params.id;
+    // console.log('Route delete. Sauce ID: ' + sauceId);
     // res.send(`<h1>Sauce supprimée</h1>`);
 }
 
@@ -56,10 +68,13 @@ exports.deleteSauce = (req, res, next) => {
  * @param {*} next  the next middleware function to execute
  */
 exports.getOneSauce = (req, res, next) => {
-    res.status(200);
-    let sauceId = req.params.id;
-    console.log('Route get one sauce "/id" reçue ! ' + sauceId);
-    res.send(`<h1>Récupérer la sauce id ${sauceId}, en travaux</h1>`);
+    Sauce.findOne({_id: req.params.id})
+    .then(sauce => res.status(200).json(sauce))
+    .catch(error => res.status(404).json({ error }));
+    // res.status(200);
+    // let sauceId = req.params.id;
+    // console.log('Route get one sauce "/id" reçue ! ' + sauceId);
+    // res.send(`<h1>Récupérer la sauce id ${sauceId}, en travaux</h1>`);
 };
 
 /**
@@ -69,8 +84,11 @@ exports.getOneSauce = (req, res, next) => {
  * @param {*} next  the next middleware function to execute
  */
 exports.getAllSauces = (req, res, next) => {
+    Sauce.find()
+    .then(sauces => res.status(200).json(sauces))
+    .catch(error => res.status(400).json({error}));
     res.status(200);
-    console.log('Route "/" reçue !');
-    res.json({ message: 'Votre requête est bien reçue'});
+    // console.log('Route "/" reçue !');
+    // res.json({ message: 'Votre requête est bien reçue'});
     // res.send('<h1>Récupérer toutes les sauces, en travaux</h1>');
 };
