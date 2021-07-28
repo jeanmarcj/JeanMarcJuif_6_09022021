51ff5ecafb60fb3c25b4722ee15c8154b8fc7ef0
jeanmarcj <jmjbup@gmail.com>
2021-07-28 09:11:28 +0200

    Impl. Add restriction on schema.

--- models/Sauce.js
+++ models/Sauce.js
@@ -1,13 +1,31 @@
 const mongoose = require('mongoose');
 
 const sauceSchema = mongoose.Schema({
-    userId: { type: String},
-    name: { type: String, required: true },
-    manufacturer: { type: String, required: true },
+    userId: { type: String },
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+    },
+    manufacturer: {
+        type: String,
+        required: true,
+        trim: true,
+    },
     description: { type: String, required: true },
-    mainPepper: { type: String, required: true },
+    mainPepper: {
+        type: String,
+        required: true,
+        trim: true,
+    },
     imageUrl: { type: String, required: true },
-    heat: { type: Number, required: true },
+    heat: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 12,
+    },
     likes: { type: Number, default: 0 },
     dislikes: { type: Number, default: 0 },
     usersLiked: [{ type: String }],
