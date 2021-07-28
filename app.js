a1aad799558f1cc4339e65b2d1e28991ce2cd802
jeanmarcj <jmjbup@gmail.com>
2021-07-28 17:00:21 +0200

    Feat. Add helmet module to protect various HTTP headers.

--- app.js
+++ app.js
@@ -3,18 +3,26 @@ const mongoose = require('mongoose');
 const path = require('path');
 const cors = require('cors');
 
+// Security
+const helmet = require('helmet');
+
+// Routing
 const sauceRoutes = require('./routes/sauce.js');
 const userRoutes = require('./routes/user.js');
 
+// Init app
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// helmet
+app.use(helmet());
+
 /**
  * Mongo DB connection
  */
-mongoose.connect('',
+mongoose.connect('mongodb+srv://sopekockoUser:ormesson94490@cluster0.bneic.mongodb.net/sopekockoDB?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -34,4 +42,4 @@ app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
   
 
-  module.exports = app;
+module.exports = app;
