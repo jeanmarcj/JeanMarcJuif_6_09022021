db27f4d3a0e18468b78df5852dd516e323dfb6b3
jeanmarcj <jmjbup@gmail.com>
2021-07-28 09:13:39 +0200

    Fix. Import order for sauceCtrl moved in last.

--- routes/sauce.js
+++ routes/sauce.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const sauceCtrl = require('../controllers/sauce');
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
+const sauceCtrl = require('../controllers/sauce');
 
 
 /**
