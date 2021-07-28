b060a72dc9b4ef65311b66dfe748a2fd9564e04e
jeanmarcj <jmjbup@gmail.com>
2021-07-28 09:09:03 +0200

    Fix. Fix error in jwt random secret token for authentification

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,8 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN-SECRET');
+        console.log(token);
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
         const userId = decodedToken.userId;
         if (req.body.userId && req.body.userId !== userId) {
             throw 'The user Id is not available';
