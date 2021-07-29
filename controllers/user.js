ca9165fcc2bee6b38d091026798828c9ad160fb1
jeanmarcj <jmjbup@gmail.com>
2021-07-29 14:21:23 +0200

    Feat. Add maskdata npm module to mask the user email inside the db

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,9 @@
+const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const User = require('../models/User');
+// Mask the user email adress inside the database
+const MaskData = require('maskdata');
 
 /**
  * User sign up
@@ -13,7 +15,7 @@ exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
     .then(hash => {
         const user = new User({
-            email: req.body.email,
+            email: MaskData.maskEmail2(req.body.email),
             password: hash,
         });
         user.save()
@@ -24,7 +26,7 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
+    User.findOne({ email: MaskData.maskEmail2(req.body.email) })
     .then(user => {
         if (!user) {
             return res.status(401).json({ error: 'User not found !'});
